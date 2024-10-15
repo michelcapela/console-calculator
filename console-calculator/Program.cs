da5f90257814802115a149547b14cfc3ed070c02
Michel Capela <michel.capela@outlook.com>
2024-10-15 13:42:36 -0300

    feat: basic arithmetic

---
+++ console-calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using console_calculator;
+
+Console.WriteLine("Hello, welcome to the calculator program!");
+
+
+try
+{
+
+    Calculator.Calculate();
+
+}
+catch (Exception e)
+{
+
+    Console.WriteLine("Please enter a valid value.");
+    Calculator.Calculate();
+}
+
+
+
+
+
+Console.ReadKey();
